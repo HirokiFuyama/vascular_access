671963e5bc20b77828963fa5fcaa4210f37752ff
hiroki <whistle.khbems.18@gmail.com>
2020-08-16 12:33:39 +0900

    make gan.py

---
+++ notebooks/gan.py
@@ -0,0 +1,125 @@
+import torch.utils.data as data
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
+
+
+class Generator(nn.Module):
+
+    def __init__(self, z_dim=50, image_size=128):
+        super(Generator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, image_size * 8,
+                               kernel_size=8, stride=1),
+            nn.BatchNorm2d(image_size * 8),
+            nn.ReLU(inplace=True))
+
+        self.layer2 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 8, image_size * 4,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size * 4),
+            nn.ReLU(inplace=True))
+
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 4, image_size * 2,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size * 2),
+            nn.ReLU(inplace=True))
+
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(image_size * 2, image_size,
+                               kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(image_size),
+            nn.ReLU(inplace=True))
+
+        self.last = nn.Sequential(
+            nn.ConvTranspose2d(image_size, 1, kernel_size=4,
+                               stride=2, padding=1),
+            nn.Tanh())
+
+    def forward(self, z):
+        out = self.layer1(z)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.last(out)
+
+        return out
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, image_size=128):
+        super(Discriminator, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, image_size, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(image_size, image_size*2, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Dropout2d(0.25)
+            )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(image_size*2, image_size*4, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Dropout2d(0.25)
+            )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(image_size*4, image_size*8, kernel_size=4,
+                      stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.25)
+            )
+
+        self.last = nn.Conv2d(image_size*8, 1, kernel_size=8, stride=1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        feature = out
+        feature = feature.view(feature.size()[0], -1)
+        out = self.last(out)
+        return out, feature
+
+
+class ImageTransform():
+    """
+    This class is preprocessing image.
+    """
+    def __init__(self, mean, std):
+        self.data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
+    def __call__(self, img):
+        return self.data_transform(img)
+
+
+class GAN_Img_Dataset(data.Dataset):
+
+    def __init__(self, file_list, transform):
+        self.file_list = file_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        """
+        Convert image to Tensor data type.
+        """
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img)
+        return img_transformed

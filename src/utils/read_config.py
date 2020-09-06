aff2571fdadbcdbfa9ce996a07dc0d73b53a4702
hiroki <whistle.khbems.18@gmail.com>
2020-09-06 21:33:47 +0900

    Fix path

--- src/utils/read_config.py
+++ src/utils/read_config.py
@@ -3,5 +3,5 @@ import configparser
 
 def read_config():
     config_ini = configparser.ConfigParser()
-    config_ini.read("/Users/hiroki/github/vascular_access/src/utils/config.ini")
+    config_ini.read("/vascular_access/src/utils/config.ini")
     return config_ini
